f2e5c807cbe4c759723e41c33aba753bf3e7b30a
Wésia Kaliany Lima Peixoto <161386123+WesiaKaliany10@users.noreply.github.com>
2025-10-07 19:13:50 -0300

    Delete src directory

--- src/main/java/com/mycompany/academiaapi/exception/BusinessRuleException.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.academiaapi.exception;
-
-/**
- * Exceção para regras de negócio.
- * @author wesia
- */
-public class BusinessRuleException extends RuntimeException {
-    public BusinessRuleException(String message) { super(message); }
-}
--- src/main/java/com/mycompany/academiaapi/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.academiaapi.exception;
-
-/**
- * Exceção para recurso não encontrado.
- * @author wesia
- */
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) { super(message); }
-}
